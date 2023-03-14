aeafc24f808fc187470aa6878b24bfeba0ff9803
Chakib Grube Benhaddou <chakib.benhaddou@external.nexigroup.com>
2023-03-14 13:48:40 +0100

    fix2

--- pages/approve/[sessionId].vue
+++ pages/approve/[sessionId].vue
@@ -6,7 +6,7 @@ const approved = ref(false);
 const swipeClick = async () => {
   const route = useRoute()
    // Default options are marked with *
-   const response = await fetch(`https://bystrup.net/mitid-admin-proxy/v1/anonymous/${route.params.sessionId}`, {
+   const response = await fetch(`https://bystrup.net/anonymous/v1/anonymous/${route.params.sessionId}`, {
     method: "POST", // *GET, POST, PUT, DELETE, etc.
     mode: "cors", // no-cors, *cors, same-origin
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
